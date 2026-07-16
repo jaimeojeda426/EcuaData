4ca2ce8e8266629511e81c2bea2ee862f5e6621c
Jaime Ojeda <jaimeojeda426@gmail.com>
2026-07-16 01:39:23 -0500

    Added MVP HTML

--- pandas_scripts.py
+++ pandas_scripts.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import plotly.express as px
+
 
 # Creating a DataFrame from a dictionary
 # DataFrame - data structure that has the keys as it's column labels, and the values as data stored in those columns, has labeled axes for rows and columns
@@ -8,6 +10,9 @@ data = {
 }
 df = pd.DataFrame(data)
 df2 = pd.read_csv('candidates - Sheet1.csv')
-print(df)
-print(df2)
+pie_fig = px.pie(df, values = 'Votes',names = 'Candidate', title = "Results")
+#print(df) #default df created via dict
+#print(df2) #df created via reading a csv
+#both produce same df
+pie_fig.show()
 
